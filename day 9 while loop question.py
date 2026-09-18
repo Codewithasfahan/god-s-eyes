eca0f7c5b5542ffa43898b3028e9b3a647a06eee
Asfahan <dasfahan49@gmail.com>
2026-09-18 12:25:04 +0530

    i add file

---
+++ day 9 while loop question.py
@@ -0,0 +1,19 @@
+# a=1
+# while(a<10):
+#     print(a)
+#     a=a+1
+    
+# a=int(input("enter your number = "))
+# b=1
+# while(b<=10):
+#     print(a,"X",b,"=",a*b)
+#     b=b+1
+a=int(input("enter your number = "))
+sum=0
+n=1
+while (n<=a):
+    sum=sum+a
+    a=a+1
+print(sum)
+
+
